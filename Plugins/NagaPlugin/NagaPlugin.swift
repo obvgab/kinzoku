676bb924367f801a95359f23641bf1f8827452a2
Gab <gabriel@binarysky.ai>
2023-09-11 13:56:17 -0700

    Primitive Xcode base, make cleaner later

--- Plugins/NagaPlugin/NagaPlugin.swift
+++ Plugins/NagaPlugin/NagaPlugin.swift
@@ -24,3 +24,31 @@ struct NagaPlugin: BuildToolPlugin {
         }
     }
 }
+
+// Because Xcode has to be special
+#if canImport(XcodeProjectPlugin)
+import XcodeProjectPlugin
+
+extension NagaPlugin: XcodeBuildToolPlugin {
+    func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
+        guard let sourceShaders = FileManager.default
+            .enumerator(atPath: context.pluginWorkDirectory.appending(subpath: "Shaders").string)? // Allow to be overriden?
+            .allObjects
+            .compactMap({ $0 as? String })
+        else {
+            return []
+        }
+        
+        return try sourceShaders.map {
+            Command.buildCommand( // Either make both .metal and .spv, or figure out our platform target
+                displayName: "Converting WGSL to Metal", // Make dynamic
+                executable: try context.tool(named: "naga").path,
+                arguments: [
+                    $0,
+                    $0.replacingOccurrences(of: ".wgsl", with: ".metal") // Make dynamic
+                ]
+            )
+        }
+    }
+}
+#endif
